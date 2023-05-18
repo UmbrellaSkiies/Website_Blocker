0709c398d16f4c2d4404bb50e50c31f5bdec9205
Neo Thomas Titebe <94100024+UmbrellaSkiies@users.noreply.github.com>
2023-05-18 19:10:17 +0200

    Add files via upload

--- webblocker.py
+++ webblocker.py
@@ -1,20 +1,25 @@
 from tkinter import *
+import tkinter as tk
 
-root = Tk()
+root = tk.Tk()
 root.geometry('500x300')
 root.resizable(0, 0)
 root.title("Website Blocker")
 
 Label(root, text='The Matrix Website Blocker', font='arial 20 bold', fg='#4a2140', pady=15).pack()
 
+# Instructions label
+instructions_label = Label(root, text='Enter the websites you want to block (separated by commas):', font='arial 12', fg='black')
+instructions_label.pack()
+
 host_path = 'C:\Windows\System32\drivers\etc\hosts'
 ip_address = '127.0.0.1'
 
-Label(root, text='Enter Website:', font='arial 13 bold').place(x=5, y=85)
-Websites = Text(root, font='arial 11', height=2, width=40)
-Websites.place(x=140, y=80)
+Label(root, text='Enter Website:', font='arial 13 bold').place(x=5, y=115)
 
-# Create status_label as a global variable
+Websites = Text(root, font='arial 11', height=2, width=40)
+Websites.place(x=140, y=110)
+
 status_label = Label(root, text="", font='arial 12 bold')
 status_label.place(x=220, y=250)
 
@@ -39,7 +44,8 @@ def unblock_website():
     with open(host_path, 'r+') as host_file:
         lines = host_file.readlines()
         host_file.seek(0)
-        blocked = False  # Flag to track if any website was unblocked
+        blocked = False
+
         for line in lines:
             if not any(web.strip() in line for web in Website):
                 host_file.write(line)
@@ -59,15 +65,12 @@ def exit_app():
     root.destroy()
 
 block = Button(root, text='Block', font='arial 12 bold', pady=5, command=block_website, width=8, bg='red', activebackground='pink')
-block.place(x=210, y=150)
+block.place(x=210, y=170)
 
 unblock = Button(root, text='Unblock', font='arial 12 bold', pady=5, command=unblock_website, width=8, bg='green', activebackground='light green')
-unblock.place(x=310, y=150)
+unblock.place(x=310, y=170)
 
 clear = Button(root, text='Clear', font='arial 12 bold', pady=5, command=clear_entry, width=6, bg='royal blue', activebackground='sky blue')
-clear.place(x=30, y=150)
-
-# exit_button = Button(root, text='Exit', font='arial 12 bold', pady=5, command=exit_app, width=6, bg='gray', activebackground='light gray')
-# exit_button.place(x=310, y=200)
+clear.place(x=30, y=170)
 
 root.mainloop()
